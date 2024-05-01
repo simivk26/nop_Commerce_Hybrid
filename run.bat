e371297925b7e4cb29797864e29e2e720a1161d9
simivk26 <159152802+simivk26@users.noreply.github.com>
2024-05-01 12:01:15 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -5,7 +5,7 @@ C:\Users\simiv\PycharmProjects\nop_Commerce_Hybrid\venv\Scripts\pytest -s -m "re
 ::C:\Users\simiv\PycharmProjects\nop_Commerce_Hybrid\venv\Scripts\pytest -s -m "sanity or regression" --html=Reports\report.html testCases\ --browser chrome
 
 ::Firefox
-C:\Users\simiv\PycharmProjects\nop_Commerce_Hybrid\venv\Scripts\pytest -s -m "regression" --html=Reports\report.html testCases\ --browser firefox
+::C:\Users\simiv\PycharmProjects\nop_Commerce_Hybrid\venv\Scripts\pytest -s -m "regression" --html=Reports\report.html testCases\ --browser firefox
 ::C:\Users\simiv\PycharmProjects\nop_Commerce_Hybrid\venv\Scripts\pytest -s -m "sanity" --html=Reports\report.html testCases\ --browser firefox
 ::C:\Users\simiv\PycharmProjects\nop_Commerce_Hybrid\venv\Scripts\pytest -s -m "sanity and regression" --html=Reports\report.html testCases\ --browser firefox
 ::C:\Users\simiv\PycharmProjects\nop_Commerce_Hybrid\venv\Scripts\pytest -s -m "sanity or regression" --html=Reports\report.html testCases\ --browser firefox
